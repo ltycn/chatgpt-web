2e90c53b1a82514ab88d84b986c0340e664b4044
tianyi <i@terry.ee>
2024-03-18 16:37:04 +0800

    Update UsageTips.tsx

--- src/app/chat/UsageTips.tsx
+++ src/app/chat/UsageTips.tsx
@@ -12,7 +12,8 @@ export function UsageTips() {
         ? [
             '❤️ 此网站仅供内部使用，请勿share给组织之外的人员',
             '⚠️ 上网不涉密，涉密不上网。请勿🚫将任何敏感信息提交到此网站',
-            '❔ 使用中有任何疑问，点击左下角Teams图标联系',
+            '❔ 使用中有任何疑问，点击左下角Teams图标，或联系liuty24',
+            '🔥 for Platform Enablement Team'
           ]
         : [
             'This website is for internal use only. Please do not share it with individuals outside the organization.',
